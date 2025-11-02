163918905f1a327d68f03c7aa236c8fde5209978
Nick <32932528+ShkiperJr@users.noreply.github.com>
2025-11-02 11:26:12 +0300

    Time out 3

--- game_tic_tac.py
+++ game_tic_tac.py
@@ -1,7 +1,6 @@
 game_matrix = [["-" for j in range(3)] for i in range(3)]
 
 
-
 def game_matrix_print(matrix):
     """A beautifully designed graphic display of the game matrix
 
@@ -39,16 +38,122 @@ def check_input(player, coordinates):
             print('Ошибка ввода!!! Введите 1, 2, 3.')     
 
     
-
 def сhecking_game_end():
-    if "-" not in game_matrix[1]:
+    """Checks the logic of the game's ending. Checks for 16 victory options. 
+       Checks for the game matrix being filled.
+    """
+    
+    def upper_cells(a1 ,b1 , a2, b2, a3, b3, index):
+        """This function raises the winning line for a more attractive 
+           display.
+
+        Args:
+            a1 (int): Win line coordinate
+            b1 (int): Win line coordinate
+            a2 (int): Win line coordinate
+            b2 (int): Win line coordinate
+            a3 (int): Win line coordinate
+            b3 (int): Win line coordinate
+            index (char): Player symbol
+        """
+        game_matrix[a1][b1] = index
+        game_matrix[a2][b2] = index
+        game_matrix[a3][b3] = index
+# Checks the logic of the game's ending.        
+    if not any ('-' in i for i in game_matrix):
         print('There is no Winner. DRAW')
+        return False
+#1 Checks for 16 victory options.   
+    if game_matrix[0][0] == game_matrix[0][1] == game_matrix[0][2] == 'x':
+        print('Player 1 WIN!!!')
+        upper_cells(0 ,0 , 0, 1, 0, 2, "Х")
+        game_matrix_print(game_matrix)
+        return False
+    if game_matrix[0][0] == game_matrix[0][1] == game_matrix[0][2] == 'o':
+        print('Player 2 WIN!!!')
+        upper_cells(0 ,0 , 0, 1, 0, 2, "O")
+        game_matrix_print(game_matrix)
+        return False
+#2
+    if game_matrix[1][0] == game_matrix[1][1] == game_matrix[1][2] == 'x':
+        print('Player 1 WIN!!!')
+        upper_cells(1 ,0 , 1, 1, 1, 2, "Х")
+        game_matrix_print(game_matrix)
+        return False
+    if game_matrix[1][0] == game_matrix[1][1] == game_matrix[1][2] == 'o':
+        print('Player 2 WIN!!!')
+        upper_cells(1 ,0 , 1, 1, 1, 2, "O")
+        game_matrix_print(game_matrix)
+        return False
+ #3   
+    if game_matrix[2][0] == game_matrix[2][1] == game_matrix[2][2] == 'x':
+        print('Player 1 WIN!!!')
+        upper_cells(2 ,0 , 2, 1, 2, 2, "Х")
+        game_matrix_print(game_matrix)
+        return False
+    if game_matrix[2][0] == game_matrix[2][1] == game_matrix[2][2] == 'o':
+        print('Player 2 WIN!!!')
+        upper_cells(2 ,0 , 2, 1, 2, 2, "O")
+        game_matrix_print(game_matrix)
+        return False  
+#4    
+    if game_matrix[0][0] == game_matrix[1][0] == game_matrix[2][0] == 'x':
+        print('Player 1 WIN!!!')
+        upper_cells(0 ,0 , 1, 0, 2, 0, "Х")
+        game_matrix_print(game_matrix)
+        return False
+    if game_matrix[0][0] == game_matrix[1][0] == game_matrix[2][0] == 'o':
+        print('Player 2 WIN!!!')
+        upper_cells(0 ,0 , 1, 0, 2, 0, "O")
+        game_matrix_print(game_matrix)
         return False 
+#5    
+    if game_matrix[0][1] == game_matrix[1][1] == game_matrix[2][1] == 'x':
+        print('Player 1 WIN!!!')
+        upper_cells(0 ,1 , 1, 1, 2, 1, "Х")
+        game_matrix_print(game_matrix)
+        return False
+    if game_matrix[0][1] == game_matrix[1][1] == game_matrix[2][1] == 'o':
+        print('Player 2 WIN!!!')
+        upper_cells(0 ,1 , 1, 1, 2, 1, "O")
+        game_matrix_print(game_matrix)
+        return False
+#6    
+    if game_matrix[0][2] == game_matrix[1][2] == game_matrix[2][2] == 'x':
+        print('Player 1 WIN!!!')
+        upper_cells(0 ,2 , 1, 2, 2, 2, "Х")
+        game_matrix_print(game_matrix)
+        return False
+    if game_matrix[0][2] == game_matrix[1][2] == game_matrix[2][2] == 'o':
+        print('Player 2 WIN!!!')
+        upper_cells(0 ,2 , 1, 2, 2, 2, "O")
+        game_matrix_print(game_matrix)
+        return False
+#7    
+    if game_matrix[0][0] == game_matrix[1][1] == game_matrix[2][2] == 'x':
+        print('Player 1 WIN!!!')
+        upper_cells(0 ,0 , 1, 1, 2, 2, "Х")
+        game_matrix_print(game_matrix)
+        return False
+    if game_matrix[0][0] == game_matrix[1][1] == game_matrix[2][2] == 'o':
+        print('Player 2 WIN!!!')
+        upper_cells(0 ,0 , 1, 1, 2, 2, "O")
+        game_matrix_print(game_matrix)
+        return False
+#8    
+    if game_matrix[0][2] == game_matrix[1][1] == game_matrix[2][0] == 'x':
+        print('Player 1 WIN!!!')
+        upper_cells(0 ,2 , 1, 1, 2, 0, "Х")
+        game_matrix_print(game_matrix)
+        return False
+    if game_matrix[0][2] == game_matrix[1][1] == game_matrix[2][0] == 'o':
+        print('Player 2 WIN!!!')
+        upper_cells(0 ,2 , 1, 1, 2, 0, "O")
+        game_matrix_print(game_matrix)
+        return False
+
     return True
-
-        
-  
-
+      
 
 def run_game():
     print("Game has begun")    
